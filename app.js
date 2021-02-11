296608de019bda4d2edb5318374349e54b25d486
bishalpandit <55312712+bishalpandit@users.noreply.github.com>
2021-02-11 17:27:22 +0530

    Added waypoint functionality

--- app.js
+++ app.js
@@ -3,3 +3,43 @@ const preload = document.querySelector('.preload');
 preload.classList.add('preload-finish');
 
 });
+
+
+$(document).ready(function(){
+  $(' .feat-part1').waypoint(function(direction){
+      $(' .feat-part1').addClass('animated fadeInLeft')
+  }, {
+
+      offset : '55%'
+  });
+
+  $(' .feat-part2').waypoint(function(direction){
+    $(' .feat-part2').addClass('animated fadeInUp')
+}, {
+
+    offset : '55%'
+});
+
+$(' .feat-part3').waypoint(function(direction){
+  $(' .feat-part3').addClass('animated fadeInRight')
+}, {
+
+  offset : '55%'
+});
+
+$(' .frame').waypoint(function(direction){
+  $(' .frame').addClass('animated zoomIn')
+}, {
+
+  offset : '55%'
+});
+
+$(' .head-img').waypoint(function(direction){
+  $(' .head-img').addClass('animated zoomIn')
+}, {
+
+  offset : '55%'
+});
+  
+  
+});
